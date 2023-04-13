9a5507d1d5b3b8d43a3407f88623e16127ea9c24
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-04-12 21:26:48 -0500

    entrega 4 completa

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,43 +1,90 @@
-//import { HemisphereLight, HemisphereLightHelper } from 'three';
-import { SpotLight, SpotLightHelper } from 'three';
+import { HemisphereLight, HemisphereLightHelper } from 'three';
+//import { SpotLight, SpotLightHelper } from 'three';
+//import { PointLight, PointLightHelper } from 'three';
+//import { RectAreaLight } from 'three';
+
 import { useRef } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import Floor from './Floor';
 import Door from './Door';
 
 export default function Experience() {
-//   const hemisphereLightRef = useRef();
 
-//   // crea la instancia del HemisphereLight
-//   const hemisphereLight = new HemisphereLight(0xffffff, 0x000000, 10);
-//   hemisphereLight.position.set(0, 2, -2);
-//   hemisphereLightRef.current = hemisphereLight;
 
-//   // crea la instancia del helper
-//   const hemisphereLightHelper = new HemisphereLightHelper(hemisphereLight, 10);
+  
+  //
 
-//   // agrega el helper a la escena
-//   hemisphereLight.add(hemisphereLightHelper);
 
-    const SpotLightRef = useRef();
+  const hemisphereLightRef = useRef();
 
-    //Crea la instancia de SpotLight
-    const spotLight = new SpotLight(0xffffff, 0x000000, 5.5);
-    spotLight.position.set(0, 2, -2);
-    SpotLightRef.current = spotLight;
+  // crea la instancia del HemisphereLight
+  const hemisphereLight = new HemisphereLight(0xffffff, 0x000000, 10);
+  hemisphereLight.position.set(0, 2, -2);
+  hemisphereLightRef.current = hemisphereLight;
 
-   // crea la instancia del helper
-    const spotLightHelper = new SpotLightHelper(spotLight, 5.5);
+  // crea la instancia del helper
+  const hemisphereLightHelper = new HemisphereLightHelper(hemisphereLight, 10);
 
-    // agrega el helper a la escena
-    spotLight.add(spotLightHelper);
+  // agrega el helper a la escena
+  hemisphereLight.add(hemisphereLightHelper);
 
 
 
+
+
+
+
+
+  //   const SpotLightRef = useRef();
+
+  //   //Crea la instancia de SpotLight
+  //   const spotLight = new SpotLight(0xffffff, 0x000000, 5.5);
+  //   spotLight.position.set(0, 2, -2);
+  //   SpotLightRef.current = spotLight;
+
+  //  // crea la instancia del helper
+  //   const spotLightHelper = new SpotLightHelper(spotLight, 5.5);
+
+  //   // agrega el helper a la escena
+  //   spotLight.add(spotLightHelper);
+
+
+
+
+  // const pointLightRef = useRef();
+
+  // //Crea la instacia de pointLight
+ 
+  // const pointLight = new PointLight(0xffffff, 0x000000, 5.5);
+  // pointLight.position.set(0, 2, -2);
+  // pointLightRef.current = pointLight;
+
+  // //Crea la instancia de helper
+  // const pointLightHelper = new PointLightHelper(pointLight, 5.5);
+
+  // //agrega el helper a la escena
+  // pointLight.add(pointLightHelper);
+
+
+  //ReactAreaLight
+
+  // const reactAreaLightRef = useRef();
+
+  // //Crea la instacia de pointLight
+ 
+  // const reactAreaLight = new RectAreaLight(0xffffff, 0x000000, 5.5);
+  // reactAreaLight.position.set(0, 2, -2);
+  // reactAreaLightRef.current = reactAreaLight;
+
+
+
+
+
+  //en objetc se reemplaza por el tipo de luz ejemplo pointLight
   return (
     <>
       <OrbitControls makeDefault />
-      <primitive object={spotLight} castShadow intensity={11}/>
+      <primitive object={hemisphereLight} castShadow intensity={11}/>
       <ambientLight intensity={0.5} />
       <Floor />
       <Door />
